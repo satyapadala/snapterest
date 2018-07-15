96dad5e1af770a13d90941c9aba3365788d92837
Satya Padala <spadala@itconvergence.com>
2018-07-16 01:07:45 +0530

    Initial commit

---
+++ source/app.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var h1 = React.createElement('h1', {className: 'header', key: 'header'},'This is React');
+var p = React.createElement('p', {className: 'content', key: 'content'},"And that's how it works.");
+var reactFragment = [h1, p];
+var section = React.createElement('section', {className: 'container'}, reactFragment)
+ReactDOM.render(
+    section,
+    document.getElementById('react-application')
+);
